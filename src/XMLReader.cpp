f319b6fb8c382df03cede41633e9b8a12ff96312
Kay264 <cecemoco@gmail.com>
2024-02-04 11:06:03 -0800

    started adding to XMLReader

---
+++ src/XMLReader.cpp
@@ -0,0 +1,24 @@
+#include "XMLReader.h"
+#include <expat.h>
+
+struct CXMLReader::SImplementation{
+    std::shared_ptr< CDataSource > DDataSource;
+    XML_Parser DXMLParser;
+
+    void XML_StartElementHandler(void *userData, const XML_Char *name, const XML_Char **atts);
+
+    SImplementation(std::shared_ptr< CDataSource > src){
+        DDataSource = src;
+        DXMLParser = XML_ParserCreate(NULL);
+        XML_SetStartElementHandler(DXMLParser, XML_StartElementHandler start);
+
+    };
+
+    bool End() const{
+
+    };
+
+    bool ReadEntity(SXMLEntity &entity, bool skipdata){
+
+    };
+};

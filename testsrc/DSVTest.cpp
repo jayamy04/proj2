5d9a6708dd5dcd2198ec3a26fd328fe476964aa9
Amy Lee <aselee@ucdavis.edu>
2024-02-05 09:54:34 -0800

    Added Tests to DSVTest.cpp

--- testsrc/DSVTest.cpp
+++ testsrc/DSVTest.cpp
@@ -3,10 +3,22 @@
 #include "gtest/gtest.h"
 
 TEST(DSVWriter, WriteRow) {
-    std::string Base = "Hello";
+    auto Sink = std::make_shared<CStringDataSink>();
+    CDSVWriter Writer(Sink,'&');
+    std::vector<std::string> input = {"Hello", "World!"};
 
-    EXPECT_EQ(StringUtils::Slice(Base,0),"Hello");
-    EXPECT_EQ(StringUtils::Slice(Base,1),"ello");
+    EXPECT_TRUE(Writer.WriteRow(input));
+    EXPECT_EQ(Sink->String(),"Hello&World!");
 }
 
-TEST(DSVWrite, )
+TEST(DSVReader, ReadRow) {
+    auto Source = std::make_shared<CStringDataSource>("Hello&World!");
+    CDSVReader Reader(Source,'&');
+    std::vector<std::string> output;
+
+    EXPECT_TRUE(Reader.ReadRow(output));
+    ASSERT_EQ(output.size(),2);
+    EXPECT_EQ(output[0],"Hello");
+    EXPECT_EQ(output[1],"World!");
+}
+

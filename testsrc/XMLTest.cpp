0ad93084fd3fbb513a485f0fe8fba5543c4da378
Kay264 <cecemoco@gmail.com>
2024-02-05 20:41:24 -0800

    xml test edits

--- testsrc/XMLTest.cpp
+++ testsrc/XMLTest.cpp
@@ -2,26 +2,28 @@
 #include "XMLWriter.h"
 #include "XMLReader.h"
 #include "StringDataSource.h"
+#include "StringDataSink.h"
+#include "gtest/gtest.h"
 
-TEST(XMLWriter, ) {
-auto OutputStream = std::make_shared<CStringDataSink>();
-CXMLWriter Writer(OutputStream);
+TEST(XMLTest, WriteEntity) {
+    auto OutputStream = std::make_shared<CStringDataSink>();
+    CXMLWriter Writer(OutputStream);
 
-EXPECT_TRUE(Writer.WriteEntity({SXMLEntity::EType::StartElement, "example", {{"attr","Hello World"}}}));
-EXPECT_TRUE(Writer.WriteEntity({SXMLEntity::EType::EndElement, "example", {}}));
+    EXPECT_TRUE(Writer.WriteEntity({SXMLEntity::EType::StartElement, "example", {{"attr","Hello World"}}}));
+    EXPECT_TRUE(Writer.WriteEntity({SXMLEntity::EType::EndElement, "example", {}}));
 
-EXPECT_EQ(OutputStream->String(), "<example attr=\"Hello World\"></example>");
+    EXPECT_EQ(OutputStream->String(), "<example attr=\"Hello World\"></example>");
 }
 
-TEST(XMLReader, ) {
-auto InputStream = std::make_shared<CStringDataSource>("<example attr=\"Hello World\"></example>");
-CXMLReader Reader(InputStream);
-SXMLEntity E;
+TEST(XMLTest, ReadEntity) {
+    auto InputStream = std::make_shared<CStringDataSource>("<example attr=\"Hello World\"></example>");
+    CXMLReader Reader(InputStream);
+    SXMLEntity E;
 
-EXPECT_TRUE(Reader.ReadEntity(E));
-EXPECT_EQ(E.DType, SXMLEntity::EType::StartElement);
-EXPECT_EQ(E.AttributeValue("attr"), "Hello World");
+    EXPECT_TRUE(Reader.ReadEntity(E));
+    EXPECT_EQ(E.DType, SXMLEntity::EType::StartElement);
+    EXPECT_EQ(E.AttributeValue("attr"), "Hello World");
 
-EXPECT_TRUE(Reader.ReadEntity(E));
-EXPECT_EQ(E.DType, SXMLEntity::EType::EndElement);
+    EXPECT_TRUE(Reader.ReadEntity(E));
+    EXPECT_EQ(E.DType, SXMLEntity::EType::EndElement);
 }
